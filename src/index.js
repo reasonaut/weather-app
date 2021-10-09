572830db1fa43566edefd71abe5faea65dce79cd
Jon Garrity <jonnygarrity@gmail.com>
2021-10-08 19:19:17 -0500

    Added city weather request functionality.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import './style.css';
 
 const apiKey = '9859218e34307ba8d4e0feb95142c9fb';
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const cityInput = document.querySelector('#city-search');
+const citySubmit = document.querySelector('button')
+const description = document.querySelector('.description');
+const city = document.querySelector('.location');
+const currentTemp = document.querySelector('.current-temp');
+const maxTemp = document.querySelector('.max-temp');
+const minTemp = document.querySelector('.min-temp');
+const humidity = document.querySelector('.humidity');
 
-const testCity = 'minneapolis';
+citySubmit.addEventListener('click', submitRequest);
+const testCity = 'Philadelphia';
 const url = `${apiUrl + testCity}&appid=${apiKey}&units=imperial`;
 console.log(url);
 
@@ -12,17 +21,39 @@ async function fetchCityData(url) {
         const data = await fetch(url).then((response) => response.json());
         return data;
     } catch (error) {
-        console.error('Error:', error);
+        return console.error('Error:', error);
     }
 }
-fetchCityData(url).then(res => console.log(processResponse(res)));
 
-
-// collect temp current, high, low
+// collect name, current temp, high, low, humidity, description from response
 function processResponse(response) {
-    let data = {};
+    const data = {};
+    data.city = response.name;
     data.currentTemp = response.main.temp;
     data.maxTemp = response.main.temp_max;
     data.minTemp = response.main.temp_min;
+    data.humidity = response.main.humidity;
+    data.description = response.weather[0].description;
     return data;
 }
+
+async function submitRequest(e) {
+    const requestCity = cityInput.value;
+    const requestUrl = `${apiUrl + requestCity}&appid=${apiKey}&units=imperial`;
+    const response = await fetchCityData(requestUrl);
+    console.log(response);
+    const data = processResponse(response);
+    updateCityWeather(data);
+
+}
+
+function updateCityWeather(data) {
+    description.textContent = data.description;
+    city.textContent = data.city;
+
+}
+
+fetchCityData(url).then(res => console.log(processResponse(res)));
+
+
+
